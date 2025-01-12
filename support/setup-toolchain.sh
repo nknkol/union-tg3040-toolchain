cd283c0369f74df80cbe513b734b724baafc7b8a
nknkol <31050940+nknkol@users.noreply.github.com>
2025-01-12 19:16:09 +0800

    Update setup-toolchain.sh

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -1,16 +1,20 @@
 #! /bin/sh
-
-TOOLCHAIN_ARCH=`uname -m`
-if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
-	exit
-fi
-
 SYSROOT_TAR="SDK_usr_tg5040_a133p"
 TOOLCHAIN_NAME="aarch64-linux-gnu"
 TOOLCHAIN_TAR="gcc-arm-8.3-2019.02-x86_64-aarch64-linux-gnu"
-
 TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.02/$TOOLCHAIN_TAR.tar.xz"
 SYSROOT_URL="https://github.com/trimui/toolchain_sdk_smartpro/releases/download/20231018/$SYSROOT_TAR.tgz"
+
+TOOLCHAIN_ARCH=`uname -m`
+if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
+	cd ~
+	wget $SYSROOT_URL
+	tar xf $SYSROOT_TAR.tgz
+	rm -rf $SYSROOT_TAR.tgz
+	exit
+fi
+
+
 
 cd ~
 
